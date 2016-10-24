27c1cb429198772363cef1f4aef958d9f5649390
Mohsin Farooqui <mohsin.farooqui4@gmail.com>
2016-10-24 12:40:34 +0530

    feeling good

--- MFPrimenumber/main.m
+++ MFPrimenumber/main.m
@@ -6,12 +6,36 @@
 //  Copyright © 2016 Mohsin Farooqui. All rights reserved.
 //
 
+void primenumber();
+
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+        
+        
+            primenumber();
+        
+        
+            }
     return 0;
+}
+void primenumber()
+{
+    int i,c;
+    int prime;
+    for (c =50; c<=150 ;c++)
+    {
+        prime = 1;
+        for (i = 2; i < c; i++)
+        {
+            if (c % i == 0)
+                prime = 0;
+        }
+        if (prime == 1)
+        {
+            printf("%d\n",c);
+        }
+        
+    }
 }
